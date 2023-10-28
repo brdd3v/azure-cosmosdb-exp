0713128422080cd36516fc6a0bf1bc78554b99af
brdd3v <brddev@yandex.ru>
2023-10-28 19:54:05 +0200

    Add pulumi variant

---
+++ pulumi/__main__.py
@@ -0,0 +1,38 @@
+"""An Azure Python Pulumi program"""
+
+from pulumi_azure import core, cosmosdb
+
+
+resource_group = core.ResourceGroup('resource_group',
+                                    name='resource-group-exp',
+                                    location='Germany West Central')
+
+account = cosmosdb.Account('account',
+                           name='account-exp',
+                           resource_group_name=resource_group.name,
+                           location=resource_group.location,
+                           offer_type='Standard',
+
+                           consistency_policy=cosmosdb.AccountConsistencyPolicyArgs(
+                               consistency_level='Strong'
+                           ),
+                           geo_locations=[
+                               cosmosdb.AccountGeoLocationArgs(
+                                   location='switzerlandnorth',
+                                   failover_priority=0
+                               )
+                           ])
+
+db = cosmosdb.SqlDatabase('sql_database',
+                          name='database-exp',
+                          resource_group_name=resource_group.name,
+                          account_name=account.name)
+
+container = cosmosdb.SqlContainer('container',
+                                  name='container-exp',
+                                  resource_group_name=resource_group.name,
+                                  account_name=account.name,
+                                  database_name=db.name,
+                                  partition_key_path='/Country',
+                                  partition_key_version=1)
+
